6805ae6514d8d180272a8799ccd624f729f18d53
DanielaMendozaA <danni.ddma.96@gmail.com>
2024-04-16 21:30:29 -0500

    Add new changes

--- Seccion_1/app2.js
+++ Seccion_1/app2.js
@@ -1,25 +1,27 @@
-    alert("Debes ingresar cada dato que corresponda a un survenir separado por coma en cada campo de datos, se deben ingresar en el mismo orden segun corresponda");
-    
-    let nombresSurvenir = prompt("Por favor ingrese los nombres de los survenir separados por coma");
-    let listaNombres = nombresSurvenir.split(",");
+let nombresSurvenir = prompt("Por favor ingrese los nombres de los survenir separados por coma");
+let listaNombres = nombresSurvenir.split(",");
+const listaCostos = [];
+const listaDisponibles = [];
 
-    listaNombres.forEach(function(nombre){
-          let listaCostos = prompt("Ingrese el costo de el survenir: " + nombre)
-          let listaDisponibilidad = confirm("Ingrese si el survenir: " + nombre + " esta disponible")
+listaNombres.forEach(function(nombre){
+      let listaCosto = prompt("Ingrese el costo de el survenir: " + nombre)
+      let listaCostoInt = parseInt(listaCosto);
+      if (!isNaN(listaCostoInt)){
+            listaCostos.push(listaCostoInt)
+      } else {
+            alert("El costo debe ser un numero")
+      }
 
-          console.log(typeof(nombre))
+      let listaDisponibilidad = confirm("Ingrese si el survenir: " + nombre + " esta disponible")
+      listaDisponibles.push(listaDisponibilidad)
+     
+});
 
-          // console.log("Datos del survenir: ")
 
-          // console.log("Nombre: " + nombre);
 
-          // console.log("Costo: " + listaCostos);
-      
-          // console.log("Disponibilidad: " + listaDisponibilidad);
 
-          
-      
-    });
+
+
 
 
 
